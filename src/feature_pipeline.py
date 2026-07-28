ff057b5c5bbf33784d7bc71d4506c72689a9346c
mjamshaid786 <bnglagame@gmail.com>
2026-07-28 18:51:25 +0500

    feauture_pipeline code update

--- src/feature_pipeline.py
+++ src/feature_pipeline.py
@@ -220,7 +220,7 @@ class FeatureEngineer:
         df["aqi_target"] = grouped[value_col].shift(-1)
         # Har city Ke Andar "Agle Din" Ki Value_col Value Nikal Kar
         # "aqi_target" column Bana Rahe Hain — Ye Future Value Hai Jo
-        # model ne predict karni hai (shift(-1) matlab ek row upar se value
+        # Model Ne predict Karni Hai (shift(-1) Matlab Ek row Upar Se Value
         # le rahe hain).
         return df
  
