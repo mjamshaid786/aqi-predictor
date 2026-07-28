67c599bacc82d883e664aeb7c87f226412544f30
mjamshaid786 <bnglagame@gmail.com>
2026-07-28 23:04:59 +0500

    feauture_pipeline code update

--- src/feature_pipeline.py
+++ src/feature_pipeline.py
@@ -250,7 +250,7 @@ class FeatureEngineer:
  
         combined = combined.dropna(subset=["aqi_target"]).reset_index(drop=True)
         # Un rows Ko Hata Rahe Hain JinKa "aqi_target" Khali (NaN) Hai —
-        # Kyunki agla din ka data na hone ki wajah se in rows ko training
+        # Kyunki Agla Din Ka data Na hone ki wajah se in rows ko training
         # ke liye use nahi kiya ja sakta. reset_index se index dobara clean
         # ho jata hai.
         return combined
