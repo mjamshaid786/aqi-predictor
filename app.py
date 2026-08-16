7b08e8582b117d78e82ec6ff6d5f8a7c2f0b2bc5
mjamshaid786 <bnglagame@gmail.com>
2026-08-16 18:36:43 +0500

    code updated

--- app.py
+++ app.py
@@ -389,7 +389,7 @@ else:
 
         # 3-Day Forecast: 3 real points (current value + day1 + day2 + day3),
         # connected with straight lines for visual continuity. These are
-        # NOT hourly-resoluti
+        # NOT hourly-res
         valid_forecast = forecast_df.dropna(subset=["predicted_aqi"])
         forecast_x = [latest.get("timestamp", recent[x_col].iloc[-1])] + list(valid_forecast["forecast_time"])
         forecast_y = [predicted_aqi] + [pm25_to_aqi(v) for v in valid_forecast["predicted_aqi"]]
